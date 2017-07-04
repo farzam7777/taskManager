723ebe42419a074e087a98b98384ea6766adeaf7
Farzam Azhar <l144211@lhr.nu.edu.pk>
2017-07-04 16:18:18 +0500

    Task controller initiated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/new'
+
+  get 'tasks/edit'
+
+  get 'tasks/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
